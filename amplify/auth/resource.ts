61f3fe65c7ed85f66ec2211d70d53036619048fc
Max <maxim.oleniukh@gmail.com>
2025-09-24 19:41:58 +0100

    vite init, amplify and bedrock set up

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,12 @@
+import { defineAuth } from "@aws-amplify/backend";
+
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to Travel Destination Generator!",
+      verificationEmailBody: (createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
+    },
+  },
+});
